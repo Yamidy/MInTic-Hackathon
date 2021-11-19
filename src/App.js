3cbaff265a6a44dd20991724584589870ccd0a17
Yamidy <dayyelaro@unal.edu.co>
2021-11-19 09:22:38 -0500

    .

--- src/App.js
+++ src/App.js
@@ -26,7 +26,17 @@ const retosList = [
    descripition:'Aquí va la descripción' },
    {title : 'Pantera Rosa',
    imgLink :'https://image.telediario.mx/sites/default/files/styles/image_1_77_1_less__2x_/public/la_pantera_rosa_celebra_su_50_aniversario.jpg',
+   descripition:'Aquí va la descripción' },
+   {title : 'Iguana',
+   imgLink :'https://th.bing.com/th/id/R.3a329b694174401e0382134d7854404c?rik=bC03mwhgI5PFEg&pid=ImgRaw&r=0',
+   descripition:'Aquí va la descripción' },
+   {title : 'Panda Rojo',
+   imgLink :'https://th.bing.com/th/id/R.588bcaf23e5ca08d7b6fd6c39d7e1417?rik=QTq2xiylu1YVNg&pid=ImgRaw&r=0',
+   descripition:'Aquí va la descripción' },
+   {title : 'Pantera Rosa',
+   imgLink :'https://image.telediario.mx/sites/default/files/styles/image_1_77_1_less__2x_/public/la_pantera_rosa_celebra_su_50_aniversario.jpg',
    descripition:'Aquí va la descripción' }
+   
 ]
 
 
